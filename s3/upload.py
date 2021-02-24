4a74e2c7543cc3f54170c450272070901818114b
NJPM <nathanminter1992@gmail.com>
2021-02-24 11:38:41 +0000

    Added shop

--- s3/upload.py
+++ s3/upload.py
@@ -19,47 +19,12 @@ def connect_client():
     s3_client = client("s3", aws_access_key_id = ACCESS_KEY, aws_secret_access_key = SECRET_ACCESS_KEY)
     return s3_client
 
-s3_client = connect_client()
-
-## Upload a file
-def upload_file(file_name, bucket="hroar.uk", object_name=None, content_type=None):
-    """
-    Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to, by default the hroar.uk bucket
-    :param object_name: S3 object name. If not specified then file_name is used
-    :param content_type: Content type, default to text/html
-    :return: True if file was uploaded, else False
-    """
-
-    ## If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = file_name
-
-    ## Upload the file
-    try:
-        if content_type is None:
-            response = s3_client.upload_file(file_name, bucket, object_name)
-        else:
-            response = s3_client.upload_file(file_name, bucket, object_name, ExtraArgs = {'ContentType': content_type})
-        print("{} uploaded.".format(object_name))
-    except ClientError as e:
-        print(e)
-        return False
-    return True
-
-"""
-## Run the upload for each file
-upload_file("/home/nathanminter1992/website/hroar.uk/dark/index.html", object_name="dark/index.html", content_type = "text/html")
-upload_file("/home/nathanminter1992/website/hroar.uk/dog/index.html", object_name="dog/index.html", content_type = "text/html")
-upload_file("/home/nathanminter1992/website/hroar.uk/images/jacato.png", object_name="images/jacato.png")
-upload_file("/home/nathanminter1992/website/hroar.uk/styles/dark.css", object_name="styles/dark.css")
-upload_file("/home/nathanminter1992/website/hroar.uk/styles/main.css", object_name="styles/main.css")
-upload_file("/home/nathanminter1992/website/hroar.uk/index.html", object_name = "index.html", content_type = "text/html")
-"""
-
+## Upload all files in hroar.uk folder
 def upload_website():
+    """
+    Upload all files in hroar.uk folder to hroar.uk bucket.
+    Objects will be named based on relative path to hroar.uk folder.
+    """
     path = "/home/nathanminter1992/website/hroar.uk/"
     for root, dirs, files in os.walk(path):
         for f in files:
@@ -72,4 +37,5 @@ def upload_website():
             print("{} uploaded.".format(object_name))
 
 if __name__ == "__main__":
+    s3_client = connect_client()
     upload_website()
