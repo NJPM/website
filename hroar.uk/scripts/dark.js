d5f8c7d4ff1717af39aa5d18e454aec597cfe733
NJPM <nathanminter1992@gmail.com>
2022-09-07 15:00:26 +0100

    Idea for remote control buttons

--- hroar.uk/scripts/dark.js
+++ hroar.uk/scripts/dark.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 jQuery(document).ready(function($){
     if (sessionStorage.getItem('over18') !== 'true') {
         $('#darkContent').hide();
@@ -22,3 +24,24 @@ jQuery(document).ready(function($){
         $('#collapseDarkRef').collapse();
     };
 });
+
+
+function startTimeout($) {
+    let passwordAttempt = window.prompt('Password');
+    if (passwordAttempt == process.env.CONTROLS_PW) {
+        let timeoutLength = parseInt(window.prompt('Put hroar in timeout for how many minutes?', '15'));
+        if (Number.isInteger(timeoutLength)) {
+            var request = new XMLHttpRequest();
+            request.open('POST', 'https://api.pushbullet.com/v2/pushes');
+            request.setRequestHeader('Access-Token', process.env.PUSH_BULLET_TOKEN);
+            request.send({type: "note", title: "timeout"});
+            window.alert(`${timeoutLength}-minute timeout sent.`);
+        }
+        else {
+            window.alert('Please enter a whole number.');
+        }
+    }
+    else {
+        window.alert('Incorrect password.')
+    }
+}
